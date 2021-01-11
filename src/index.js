2d4fcd31071adc39671d02c96fe5b9b3e6a091e1
Alena Zhmurko <alenazhmurko@gmail.com>
2021-01-11 09:01:44 +0200

    add login functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import ReactDOM from 'react-dom';
 import React, {Component} from 'react';
 import { w3cwebsocket, w3cwebsocket as W3WebSocket } from "websocket";
+import { Card, Avatar, Input, Typography } from 'antd';
+import 'antd/dist/antd.css';
+
+const { Search } = Input;
 
 const client = new w3cwebsocket("ws://127.0.0.1:8000");
 
 export default class App extends Component {
+
+  state = {
+    userName: '',
+    isLoggedIn: false
+  }
 
   onButtonClicked = (value) => {
     client.send(JSON.stringify({
@@ -25,8 +34,20 @@ export default class App extends Component {
   }
   render(){
     return (
-      <div>
-        <button onClick={() => this.onButtonClicked("Hello")}>Send message</button>
+      <div className="main">
+        {this.state.isLoggedIn ?
+          <button onClick={() => this.onButtonClicked("Hello")}>Send message</button>
+        :
+          <div style={{padding: '30vw 15vw'}}>
+            <Search 
+              placeholder="Enter Username"
+              enterButton="Login"
+              size="large"
+              onSearch={value => this.setState({isLoggedIn: true, userName: value})}
+            />
+          </div>
+        }
+        
       </div>
     )
   }
