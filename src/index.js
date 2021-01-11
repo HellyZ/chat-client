482cd4b59d54a7e954d6a3aca2579eda4160d657
Alena Zhmurko <alenazhmurko@gmail.com>
2021-01-11 08:39:09 +0200

    sending message

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import ReactDOM from 'react-dom';
 import React, {Component} from 'react';
+import { w3cwebsocket, w3cwebsocket as W3WebSocket } from "websocket";
+
+const client = new w3cwebsocket("ws://127.0.0.1:8000");
 
 export default class App extends Component {
+
+  onButtonClicked = (value) => {
+    client.send(JSON.stringify({
+      type: "message",
+      msg: value
+    }));
+  }
+
+  componentDidMount(){
+    client.onopen = () => {
+      console.log('WebSocket Client Connected');
+    };
+
+    client.onmessage = (message) => {
+      const dataFromServer = JSON.parse(message.data);
+      console.log('got reply! ', dataFromServer);
+    };
+  }
   render(){
     return (
       <div>
-        Testing the app
+        <button onClick={() => this.onButtonClicked("Hello")}>Send message</button>
       </div>
     )
   }
